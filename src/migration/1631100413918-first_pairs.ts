19f8b8209f7ab4b1b1875eb7907b62ba9001d72e
WhidRubeld <whidrubeld@gmail.com>
2021-09-09 14:47:37 +0300

    upgrade migration

--- src/migration/1631100413918-first_pairs.ts
+++ src/migration/1631100413918-first_pairs.ts
@@ -1,7 +1,36 @@
 import { getRepository, MigrationInterface, QueryRunner } from 'typeorm'
 import { Pair } from '../entity/Pair'
 
-const data = [{ symbol: 'ETHUSDT' }, { symbol: 'BTCUSDT' }]
+const data = [
+  { symbol: 'BTCUSDT' },
+  { symbol: 'ETHBTC' },
+  { symbol: 'ETHUSDT' },
+  { symbol: 'LTCBTC' },
+  { symbol: 'LTCUSDT' },
+  { symbol: 'BNBETH' },
+  { symbol: 'BNBBTC' },
+  { symbol: 'BNBUSDT' },
+  { symbol: 'ETCBTC' },
+  { symbol: 'ETCUSDT' },
+  { symbol: 'NEOBTC' },
+  { symbol: 'IOTABTC' },
+  { symbol: 'QTUMBTC' },
+  { symbol: 'ADABTC' },
+  { symbol: 'ATOMBTC' },
+  { symbol: 'BATBTC' },
+  { symbol: 'DOTBTC' },
+  { symbol: 'IOSTBTC' },
+  { symbol: 'ONTBTC' },
+  { symbol: 'BCHBTC' },
+  { symbol: 'BCHUSDT' },
+  { symbol: 'EOSBTC' },
+  { symbol: 'XMRBTC' },
+  { symbol: 'XMRUSDT' },
+  { symbol: 'ZECBTC' },
+  { symbol: 'ZECUSDT' },
+  { symbol: 'DASHBTC' },
+  { symbol: 'DASHUSDT' }
+]
 const pairs = data.map((v) => {
   const pair = new Pair()
   pair.symbol = v.symbol
